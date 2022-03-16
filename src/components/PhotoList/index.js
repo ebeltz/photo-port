531d7207706c040549808da1c3805a21d19f2e40
ebeltz <ebbeltz@gmail.com>
2022-03-16 18:44:59 -0400

    deploy website

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -105,13 +105,15 @@ const PhotoList = ({ category }) => {
   const currentPhotos = photos.filter((photo) => photo.category === category);
   const toggleModal = (image, i) => {
     setCurrentPhoto({...image, index: i});
-    setIsModalOpen(true);
+    setIsModalOpen(!isModalOpen);
   }
   const [currentPhoto, setCurrentPhoto] = useState();
 
   return (
     <div>
-      {isModalOpen && <Modal currentPhoto={currentPhoto} />}
+      {isModalOpen && (
+        <Modal currentPhoto={currentPhoto} onClose={toggleModal} />
+      )}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
